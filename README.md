194db49d10007bc93e4effafee4bc985c2e15a43
Nathen Harvey <nathen.harvey@gmail.com>
2011-12-30 11:24:57 -0500

    Update documentation about the cookbook

--- README.md
+++ README.md
@@ -1,12 +1,41 @@
 Description
 ===========
 
+GreenScreen is a build monitoring tool that is designed to be used as a dynamic Big Visible Chart (BVC) in your work area. It lets you add links to your build servers and displays the largest possible information on a monitor so that the team can see the build status from anywhere in the room.
+
+This cookbook configures a node to run one or more GreenScreen applications.  The cookbook uses [CustomInk's clone of the original GreenScreen project](git://github.com/customink/greenscreen.git).
+
 Requirements
 ============
+
+### Cookbooks:
+
+Opscode Cookbooks (http://community.opscode.com/cookbooks)
+
+* git 
+
 
 Attributes
 ==========
 
+* `greenscreen/install_dir` - Location where the GreenScreen app will run
+* `greenscreens` - An array of GreenScreen configuration parameters.  One entry per GreenScreen
+* `greenscreens/name` - The name of this GreenScreen
+* `greenscreens/port` - The port used by this GreenScreen
+* `greenscreens/servers` - An array of servers that this GreenScreen should watch. One entry per server
+* `greenscreens/servers/url` - The URL for this build server
+* `greenscreens/servers/username` - The login for this server
+* `greenscreens/servers/password` - The password for this server
+* `greenscreens/servers/jobs` - Array of jobs to look after.  Leave empty to watch all jobs on this server
+* `greenscreens/servers/ignore_jobs` - Array of jobs to ignore.  Leave empty to watch all jobs on this server
+
+
 Usage
 =====
+
+Include the greenscreen recipe to install, configure, and run one or more GreenScreen applications
+
+    include_recipe "greenscreen"
+
+Or add it to your role, or directly to a node's recipes.
 
--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,55 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 depends "git"
+
+recipe            "default", "Installs, configures, and runs a GreenScreen application."
+
+attribute "greenscreen/install_dir",
+  :display_name => "GreenScreen Installation Directory",
+  :description => "Location where the GreenScreen app will run",
+  :default => "/opt"
+
+attribute "greenscreens",
+  :display_name => "GreenScreens",
+  :description => "An array of GreenScreen configuration parameters.  One entry per GreenScreen",
+  :default => "A single GreenScreen"
+
+attribute "greenscreens/name",
+  :display_name => "GreenScreen Name",
+  :description => "The name of this GreenScreen",
+  :default => "greenscreen"
+
+attribute "greenscreens/port",
+  :display_name => "GreenScreen Port",
+  :description => "The port used by this GreenScreen",
+  :default => "/4567"
+
+attribute "greenscreens/servers",
+  :display_name => "Servers",
+  :description => "An array of servers that this GreenScreen should watch. One entry per server",
+  :default => "A single server"
+
+attribute "greenscreens/servers/url",
+  :display_name => "Server URL",
+  :description => "The URL for this build server",
+  :default => "http://ci.jenkins-ci.org/view/All/cc.xml"
+
+attribute "greenscreens/servers/username",
+  :display_name => "Server Username",
+  :description => "The login for this server",
+  :default => ""
+
+attribute "greenscreens/servers/password",
+  :display_name => "Server Password",
+  :description => "The password for this server",
+  :default => ""
+
+attribute "greenscreens/servers/jobs",
+  :display_name => "Server Jobs to Watch",
+  :description => "Array of jobs to look after.  Leave empty to watch all jobs on this server",
+  :default => ""
+
+attribute "greenscreens/servers/ignore_jobs",
+  :display_name => "Server Jobs to Ignore",
+  :description => "Array of jobs to ignore.  Leave empty to watch all jobs on this server",
+  :default => ""
